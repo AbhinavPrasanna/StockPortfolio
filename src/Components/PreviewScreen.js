b6167a9793beaeaf734393cbebc77a77ec2c6e4e
AbhinavPrasanna <aprasanna@ucmerced.edu>
2023-12-05 15:02:32 -0800

    Updated Frontend

---
+++ src/Components/PreviewScreen.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import Card from './Card'
+import Carousel from 'react-bootstrap/Carousel';
+import '../Stylesheets/Preview.css'
+
+function PreviewScreen() {
+    return (
+        <div>
+            <Carousel controls={false}>
+                <Carousel.Item>
+                    <button class="w-100 TransparentButton">
+                        <img src="https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/sapphirereservecard.png" alt="First Slide" class="w-25" />
+                    </button>
+                    <Carousel.Caption>
+                        <h3>Chase Sapphire Reserve</h3>
+                        <p>10x Points on Hotels and Dining and 5x on Flights, 50% Bonus</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <button class="w-100 TransparentButton">
+                        <img src="https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/freedomunlimitedcard.png" alt="Second Slide" class="w-25" />
+                    </button>
+                    <Carousel.Caption>
+                        <h3>Chase Freedom Unlimited</h3>
+                        <p>$200 Bonus after you spend $500 in the first 3 months</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <button class="w-100 TransparentButton">
+                        <img src="https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/sapphirepreferredcard.png" alt="Third Slide" class="w-25" />
+                    </button>
+                    <Carousel.Caption>
+                        <h3>Chase Sapphire Preferred</h3>
+                        <p>60,000 Bonus Points after you spend $4,000 in the first 3 months</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <button class="w-100 TransparentButton">
+                        <img src="https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/wfactivecash.png" alt="Fourth Slide" class="w-25" />
+                    </button>
+                    <Carousel.Caption>
+                        <h3>Wells Fargo Active Cash</h3>
+                        <p>2% Cash Back Flat</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </div>
+    )
+}
+
+export default PreviewScreen
--- src/Components/RatingScreen.js
+++ src/Components/RatingScreen.js
@@ -5,13 +5,13 @@ import RowScreen from './RowScreen'
 function RatingScreen() {
   const cards = [
     new Card(1,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/sapphirepreferredcard.png","Chase Sapphire Preferred Card", "Personal", "Chase",true,5.0),
-    new Card(2,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/citicustomcash.png", "Citi Custom Cash", "Personal", "Citi",true,4.5),
+    new Card(2,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/citicustomcash.png", "Citi Custom Cash", "Personal", "Citi",true,5.0),
     new Card(3,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/freedomunlimitedcard.png", "Chase Freedom Unlimited", "Personal", "Chase",false,5.0),
     new Card(4,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/inkpreferredcard.png", "Chase Ink Preferred Card", "Buisness", "Chase",false,4.0),
     new Card(5,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/inkcash.png", "Chase Ink Cash Card", "Buisness", "Chase",false,5.0),
     new Card(6,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/inkunlimitedcard.png", "Chase Ink Unlimited Card", "Buisness", "Chase",false,5.0),
-    new Card(8, "https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/costcoanywherecard.png","Costco Anywhere Visa Card", "Personal", "Citi",true,4.8),
-    new Card(9,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/amexplatinum.png","Amex Platnium Card", "Personal", "Amex",true,3.5),
+    new Card(8, "https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/costcoanywherecard.png","Costco Anywhere Visa Card", "Personal", "Citi",true,5.0),
+    new Card(9,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/amexplatinum.png","Amex Platnium Card", "Personal", "Amex",true,3.0),
     new Card(10,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/amexbluecashpreferred.jpg","Amex Blue Cash Preferred", "Personal", "Amex",true,4.0),
     new Card(11,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/amexbluecash.jpg","Amex Blue Card", "Personal", "Amex",false,4.5),
     new Card(12,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/venturexcard.jpg","Capital One Venture Card", "Personal", "Capital One",4.5),
@@ -20,17 +20,23 @@ function RatingScreen() {
     new Card(15,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/primevisacard.png","Chase Prime Visa Card", "Personal", "Chase",true,4.8),
     new Card(16,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/wellsfargoautograph.png","Wells Fargo Autograph", "Personal", "Wells Fargo",false,4.3),
     new Card(17,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/wellsfargoreflect.png","Wells Fargo Reflect", "Personal", "Wells Fargo",false,4.3),
-    new Card(18,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/wfactivecash.png","Wells Fargo Active Cash", "Personal", "Wells Fargo",false,4.3),
+    new Card(18,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/wfactivecash.png","Wells Fargo Active Cash", "Personal", "Wells Fargo",false,5.0),
     new Card(19,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/freedomflexcard.png","Chase Freedom Flex","Personal","Chase",false,5.0),
     new Card(20,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/mariottbonvoypremier.png","Mariott Bonvoy Premier","Personal","Chase",true,3.5),
-    new Card(21,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/hiltonhonors.jpg","Hilton Honors","Personal","Chase",true,3.5),
-    new Card(22,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/hiltonhonorsamex.png","Hilton Honors","Personal","Chase",true,3.5),
+    new Card(21,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/hiltonhonors.jpg","Hilton Honors","Personal","Amex",true,3.5),
+    new Card(22,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/hiltonhonorsamex.png","Hilton Honors","Personal","Amex",true,3.5),
     new Card(23,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/ihgpremiercard.png","IHG Rewards Card","Personal","Chase",true,3.5),
+    new Card(24,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/sapphirereservecard.png","Chase Sapphire Reserve Card","Personal","Chase",true,5.0),
+    new Card(25,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/discoverit.jpeg","Discover IT Card","Personal","Discover",true,3.5),
+    new Card(25,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/freedom_rise_card.png","Chase Freedom Rise","Personal","Chase",true,3.5),
+    new Card(26,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/slate_edge_card.png", "Chase Slate Edge", "Personal", "Chase",true,3.0),
+    new Card(27,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/bankamericard.png", "Bank of America BankAmericard", "Personal", "Bank of America",true,3.0),
+    new Card(27,"https://abhinav-credit-card-images-2.s3.us-west-1.amazonaws.com/citipremiercard.png", "Citi Premier Card", "Personal", "Citi",true,3.5),
       ];
     const TopRated = cards.filter(card => card.rating === 5.0);
     const HighRated = cards.filter(card => card.rating >= 4.0 && card.rating < 5.0);
-    const MediumRated = cards.filter(card => card.rating >= 3.0 && card.rating < 4.0);
-    const LowRated = cards.filter(card => card.rating < 3.0);
+    const MediumRated = cards.filter(card => card.rating >= 3.5 && card.rating < 4.0);
+    const LowRated = cards.filter(card => card.rating < 3.5);
 
   return (
     <div>
