d4053b898b4b07be5df3729169577d194e956a88
AbhinavPrasanna <aprasanna@ucmerced.edu>
2023-12-05 20:26:29 -0800

    Added First Changes to Sign Up and Login Screen

--- src/Components/LoginScreen.js
+++ src/Components/LoginScreen.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+import {validEmail} from '../Utils/Regex';
 
 function LoginScreen() {
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    if(validEmail.test(e.target.value)) {
+        setEmail(e.target.value);
+    }
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  }
+
   return (
-    <div>LoginScreen</div>
+    <div class="container-md">
+      <div class="form-group">
+      <label for="exampleInputEmail1">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={handleEmailChange}/>
+      <small id="emailHelp" class="form-text text-muted">Must be a proper email</small>
+     </div>
+     <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" onChange={handlePasswordChange}/>
+      <small id="passwordHelp" class="form-text text-muted">Must be 8 Characters Long. Contains at least 1 number. Contains at least 1 special character. Contains at least 1 uppercase letter. Contains at least 1 lowercase letter.</small>
+     </div>
+     <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
   )
 }
 
--- src/Components/SignUpScreen.js
+++ src/Components/SignUpScreen.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {useNavigate} from 'react-router-dom'
 
-import {validEmail, validFirstName, validLastName, validPassword} from '../Utils/Regex';
+import {validEmail, validFirstName, validLastName} from '../Utils/Regex';
 
 function SignUpScreen() {
+  const [email,setEmail] = useState("");
+  const [firstName,setFirstName] = useState("");
+  const [lastName,setLastName] = useState("");
+  const [password,setPassword] = useState("");
+  const handleEmailChange = (e) => {
+    if(validEmail.test(e.target.value)) {
+        setEmail(e.target.value);
+    }
+  }
+  const handleFirstNameChange = (e) => {
+    if(validFirstName.test(e.target.value)) {
+      setFirstName(e.target.value);
+    }
+  }
+  const handleLastNameChange = (e) => {
+    if(validLastName.test(e.target.value)) {
+      setLastName(e.target.value);
+    }
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  }
+
   return (
-    <div>SignUpScreen</div>
+    <div class="container-lg">
+     <div class="form-group">
+      <label for="exampleInputEmail1">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={handleEmailChange}/>
+      <small id="emailHelp" class="form-text text-muted">Must be a proper email</small>
+     </div>
+     <div class="form-group">
+      <label for="exampleInputFirstName1">First Name</label>
+      <input type="text" class="form-control" id="exampleInputFirstName1" onChange={handleFirstNameChange}/>
+     </div>
+     <div class="form-group">
+      <label for="exampleInputLastName1">Last Name</label>
+      <input type="text" class="form-control" id="exampleInputLastName1" onChange={handleLastNameChange}/>
+     </div>
+     <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" onChange={handlePasswordChange}/>
+      <small id="passwordHelp" class="form-text text-muted">Must be 8 Characters Long. Contains at least 1 number. Contains at least 1 special character. Contains at least 1 uppercase letter. Contains at least 1 lowercase letter.</small>
+     </div>
+     <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
   )
 }
 
