21a44ddd6af53c68db4f8728f346dacf4c954b64
Abhinav Prasanna <abhinav2k01@gmail.com>
2023-12-11 19:25:47 -0800

    Update LoginScreen.js

--- src/Components/LoginScreen.js
+++ src/Components/LoginScreen.js
@@ -1,10 +1,16 @@
 import React, {useState} from 'react'
+import { useAuth } from '../Contexts/AuthContext';
+import {Auth} from 'aws-amplify';
+import { useNavigate } from 'react-router-dom';
 
 import {validEmail} from '../Utils/Regex';
 
 function LoginScreen() {
+  const {setAuthUser,setIsLoggedIn} = useAuth();
+  const navigate = useNavigate();
   const [email,setEmail] = useState("");
   const [password,setPassword] = useState("");
+  const [message,setMessage] = useState("Must be 8 Characters Long. Contains at least 1 number. Contains at least 1 special character. Contains at least 1 uppercase letter. Contains at least 1 lowercase letter.");
 
   const handleEmailChange = (e) => {
     if(validEmail.test(e.target.value)) {
@@ -14,6 +20,16 @@ function LoginScreen() {
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
+  }
+
+  const login = (e) => {
+    Auth.signIn(email,password).then(() => {
+      setIsLoggedIn(true);
+      navigate("/");
+    }
+    ).catch((error) => {
+      setMessage(error.message);
+    });
   }
 
   return (
@@ -26,9 +42,9 @@ function LoginScreen() {
      <div class="form-group">
       <label for="exampleInputPassword1">Password</label>
       <input type="password" class="form-control" id="exampleInputPassword1" onChange={handlePasswordChange}/>
-      <small id="passwordHelp" class="form-text text-muted">Must be 8 Characters Long. Contains at least 1 number. Contains at least 1 special character. Contains at least 1 uppercase letter. Contains at least 1 lowercase letter.</small>
+      <small id="passwordHelp" class="form-text text-muted">{message}</small>
      </div>
-     <button type="submit" class="btn btn-primary">Submit</button>
+     <button type="submit" class="btn btn-primary" onClick={login}>Submit</button>
     </div>
   )
 }
