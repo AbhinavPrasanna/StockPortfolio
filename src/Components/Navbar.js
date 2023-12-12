38ea9717da791e655c149eb6dc72cd34ab07a161
Abhinav Prasanna <abhinav2k01@gmail.com>
2023-12-11 19:26:27 -0800

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,11 +3,22 @@ import React, { useEffect, useState } from 'react'
 import '../Stylesheets/Navbar.css';
 import '../Stylesheets/Buttons.css';
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../Contexts/AuthContext';
+import { Auth } from 'aws-amplify';
 
 function Navbar() {
+    const {authUser,setAuthUser, isLoggedIn, setIsLoggedIn} = useAuth();
     const navigate = useNavigate();
+
+    const hidden = { display : "none" };
+    const visible = { display : "block" };
+
     const [searchWord, setSearchWord] = useState("");
+    const [signUpView, setSignUpView] = useState(visible);
+    const [loginView, setLoginView] = useState(hidden);
+
     useEffect(() => {
+        console.log("useEffect");
         if(searchWord !== "" && searchWord) {
             navigate("/search",{state: {searchWord: searchWord}})
         }
@@ -17,6 +28,17 @@ function Navbar() {
         if(e.target.value !== '' && e.target.value) {
             setSearchWord(e.target.value);
         }
+    }
+
+    const handleLogout = (e) =>{
+      Auth.signOut().then(() => {
+        setIsLoggedIn(false);
+        setSignUpView(visible);
+        setLoginView(hidden);
+      }
+        ).catch((error) => {
+            console.log(error);
+        });
     }
     return (
         <div class="bar">
@@ -31,8 +53,18 @@ function Navbar() {
                     <button class="nav-item TransparentButton2 TabText ms-5" onClick={() => navigate("/forms")}>Forms</button>
                     <input class="nav-item TransparentButton2 TabText" onChange={handleSearch}placeholder="Search"/>
                 </div>
-                <button class="nav-item SignUpButtonLayout SignUpText me-5" onClick={() => navigate("/signup") }>Sign Up</button>
-                <button class="nav-item TransparentButton2 TabText me-5" onClick={() => navigate("/login")}>Login</button>
+                {
+                    isLoggedIn 
+                    ?
+                    <button class="nav-item SignUpButtonLayout SignUpText me-5" onClick={handleLogout}>Logout</button>
+                    :
+                    <div>
+                        <button class="nav-item SignUpButtonLayout SignUpText me-5" onClick={() => navigate("/signup") }>Sign Up</button>
+                        <button class="nav-item TransparentButton2 TabText me-5" onClick={() => navigate("/login")}>Login</button>
+                    </div>
+
+                }
+                
             </nav>
         </div>
     )
